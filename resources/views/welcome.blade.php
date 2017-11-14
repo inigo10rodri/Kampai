06e3e1bc7d08e49cef193695952ccf87686cabd2
gondhalf <gondhalf@git.com>
2017-11-14 11:36:58 +0100

    primer comit asier

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,15 +29,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bottom’s app!
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('register') }}">¡Encuentra lo que buscas!</a>
+
+                </div>
+                <div id="pensar">
+
                 </div>
             </div>
         </div>
