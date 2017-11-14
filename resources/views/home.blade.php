0741a377c249b64a17abdd8098ace0771dc5c7a0
gondhalf <gondhalf@git.com>
2017-11-14 09:31:32 +0100

    primer comit asier

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     You are logged in!
+                    <a href="/mapa">click para el mapa</a>
                 </div>
             </div>
         </div>
